e154ddcacbfeefe251c47b540b9075ef8713bc10
Macbook <macbook@MacBook-Air-2.local>
2017-03-26 15:53:08 +0530

    first chapter complete

---
+++ app/src/main/java/com/example/macbook/firstapplication/Question.java
@@ -0,0 +1,30 @@
+package com.example.macbook.firstapplication;
+
+/**
+ * Created by macbook on 26/03/17.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
